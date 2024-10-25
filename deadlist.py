92d0fe4fb210a34f16a520a151c7da116d07d1ae
pui668 <138620408+pui668@users.noreply.github.com>
2024-10-25 05:04:16 +0200

    Update deadlist.py

--- deadlist.py
+++ deadlist.py
@@ -1,11 +1,15 @@
 import streamlit as st
 
+# Function to load the HTML file
 def load_html_file(file_path):
-    with open(r"C:\Users\puipu\Downloads\MyFlow\deadlist.html", 'r', encoding='utf-8') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
 
+# Streamlit app title
 st.title('死期到了')
 
+# Load the HTML content from the relative file path
 html_content = load_html_file('deadlist.html')
 
+# Display the HTML content in Streamlit
 st.components.v1.html(html_content, height=600, scrolling=True)
